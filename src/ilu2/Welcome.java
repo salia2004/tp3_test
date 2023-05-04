e34850317e397c4279f148325eddd59fcc7961b2
salia2004 <fatoumatasalia82@gmail.com>
2023-05-04 14:19:32 +0200

    iter2

--- src/ilu2/Welcome.java
+++ src/ilu2/Welcome.java
@@ -4,10 +4,34 @@ public class Welcome {
 	
 	
 	public static String welcome(String input) {
+		System.out.println(input.trim());
 		StringBuilder res = new StringBuilder();
 		res.append("Hello,");
 		if(input.equals("bob"))
 			res.append("Bob");
+		
+		if(isempty(input))
+		{
+			res.append("my friend");
+		}
+		input = input.trim().replaceAll("\\s+", " ");
 		return res.toString();
+	}
+	
+	/*private static boolean iscar(String car)
+	{
+		boolean res=false;
+		if((car.equals(" "))||(car.equals(null)))
+		{
+			res=true;
+		}
+		return res;
+	}*/
+	public static boolean isempty(String input) {
+		boolean res=false;
+		if (input.equals(" ") || input==null || input.equals("  ")) {
+			res=true;
+		}
+		return res;
 	}
 }
--- test/ilu2/WelcomeTest.java
+++ test/ilu2/WelcomeTest.java
@@ -10,12 +10,19 @@ class WelcomeTest {
 	@BeforeEach
 	public void init()
 	{
-		System.out.println("Debut test tp3 ..");
+		System.out.println("Debut test ");
 	}
 	@Test
-	void test() {
+	void test1() {
 		//fail("Not yet implemented");
 		assertEquals(Welcome.welcome("bob"),"Hello,Bob");
+		System.out.println("Test 1 OK");
+	}
+	@Test
+	void test2() {
+		assertEquals(Welcome.welcome("bob"),"Hello,Bob");
+		assertEquals(Welcome.welcome(" "),"Hello,my friend");
+		assertEquals(Welcome.welcome("  "),"Hello,my friend");
 	}
 
 }
